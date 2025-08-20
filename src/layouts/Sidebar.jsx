e0b9b3991c79e4bf276e0c7fd041e6dc3828fe58
Azril Rojaan <endogcity@gmail.com>
2025-08-19 22:57:04 -0600

    Mengganti function biasa menjadi arrow function dan styling dashboard page

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { forwardRef } from "react";
+import { cn } from "../utils/cn";
+import logoDark from "../assets/favicon-dark.svg";
+import logoLight from "../assets/favicon-light.svg";
 
-export default function Sidebar() {
-  return <aside>Sidebar</aside>;
-}
+const Sidebar = forwardRef(({ collapsed }, ref) => {
+  return (
+    <aside
+      ref={ref}
+      className={cn(
+        "fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white transition-[width,left,background-color,border] duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] dark:border-slate-700 dark:bg-slate-900",
+      )}
+    >
+      <div className="flex gap-x-3 p-3">
+        <img src={logoLight} alt="Logo" className="h-6 w-6 dark:hidden" />
+        <img src={logoDark} alt="Logo" className="h-6 w-6 dark:block" />
+        {!collapsed && (
+          <p className="text-lg font-medium text-slate-900 transition-colors dark:text-slate-50">
+            Uangku
+          </p>
+        )}
+      </div>
+      <div className="p-3[scrollbar-width: thin] [scrollbar-color: #cbd5e1 transparent] dark:[scrollbar-color: #334155 transparent] flex w-full flex-col gap-y-4 overflow-x-hidden overflow-y-auto"></div>
+    </aside>
+  );
+});
+
+Sidebar.displayName = "Sidebar";
+
+export default Sidebar;
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,9 +1,27 @@
+import { Outlet } from "react-router-dom";
+import Header from "../layouts/Header";
 import Sidebar from "../layouts/Sidebar";
+import { cn } from "../utils/cn";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { useRef, useState } from "react";
 
-export default function Layout() {
+const Layout = () => {
+  const isDesktopDevice = useMediaQuery("(min-width: 768px)");
+  const [collapsed, setCollapsed] = useState(!isDesktopDevice);
+
+  const sidebarRef = useRef(null);
+
   return (
-    <div className="dark:bg-slate-900min-h-screen bg-slate-100 transition-colors">
-      <Sidebar />
+    <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
+      <Sidebar ref={sidebarRef} collapsed={collapsed} />
+      <div className={cn("transition-[margin] duration-300")}>
+        <Header />
+        <div className="h-[calc(100vh-60px)] overflow-x-hidden overflow-y-auto p-6">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Layout;
