bdc97f242f2b7eda173467c8f8a20b2e0f2f817e
Azril Rojaan <endogcity@gmail.com>
2025-09-07 14:37:01 -0600

    feat(logout) : membuat popup logout

--- src/authentication/LogoutButton.jsx
+++ src/authentication/LogoutButton.jsx
@@ -2,10 +2,12 @@ import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
 import { NavLink } from "react-router-dom";
 import { cn } from "../utils/cn";
-import { LogOut } from "lucide-react";
+import { LogOut, X } from "lucide-react";
+import { useState } from "react";
 
 const LogoutButton = ({ collapsed }) => {
-  const logout = async () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleLogout = async () => {
     try {
       await signOut(auth);
     } catch (error) {
@@ -19,16 +21,56 @@ const LogoutButton = ({ collapsed }) => {
           Akun
         </p>
       </nav>
-      <NavLink
+      <button
         className={cn(
-          "flex h-[40px] w-full flex-shrink-0 items-center gap-x-3 rounded-lg p-3 text-base font-medium text-slate-900 transition-colors hover:bg-blue-50 dark:text-slate-50 dark:hover:bg-blue-950",
+          "flex h-[40px] w-full flex-shrink-0 cursor-pointer items-center gap-x-3 rounded-lg p-3 text-base font-medium text-slate-900 transition-colors hover:bg-blue-50 dark:text-slate-50 dark:hover:bg-blue-950",
           collapsed && "md:w-[45px]",
         )}
-        onClick={logout}
+        onClick={() => setShowModal(true)}
       >
         <LogOut size={22} className="flex-shrink-0" />
         {!collapsed && <p className="whitespace-nowrap">Logout</p>}
-      </NavLink>
+      </button>
+
+      {showModal && (
+        <div
+          onClick={() => setShowModal(false)}
+          className="fixed inset-0 flex items-center justify-center bg-black/50"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="w-65 rounded-xl bg-white p-6 shadow-lg md:w-80 dark:bg-slate-900"
+          >
+            <div className="flex justify-between">
+              <h2 className="mb-4 text-lg font-semibold dark:text-slate-50">
+                Konfirmasi
+              </h2>
+              <X
+                size={27}
+                className="cursor-pointer items-center dark:text-slate-50"
+                onClick={() => setShowModal(false)}
+              />
+            </div>
+            <p className="mb-6 dark:text-slate-50">
+              Apakah kamu yakin ingin keluar?
+            </p>
+            <div className="flex justify-between">
+              <button
+                onClick={() => setShowModal(false)}
+                className="cursor-pointer rounded-lg bg-slate-800 px-4 py-2 text-slate-50 transition-all hover:opacity-80 dark:bg-slate-500"
+              >
+                Batal
+              </button>
+              <button
+                onClick={handleLogout}
+                className="cursor-pointer rounded-lg bg-red-500 px-4 py-2 text-slate-50 hover:bg-red-400"
+              >
+                Keluar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
